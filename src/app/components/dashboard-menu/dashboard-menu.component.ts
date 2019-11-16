7068cee881aa4ca8bad4054a286b35c45e7199ce
shruthigopinath <shruthigopinath2@users.noreply.github.com>
2019-11-16 17:54:33 +0530

    Updated files

--- src/app/components/dashboard-menu/dashboard-menu.component.ts
+++ src/app/components/dashboard-menu/dashboard-menu.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardMenuComponent implements OnInit {
 
+  show_dashboard : boolean = false
+  show_membership_details : boolean = false
+  show_transaction_history : boolean = false
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showMenuOption(option){
+    if(option == 'dashboard')
+    {
+      this.show_dashboard = true
+      this.show_membership_details = false
+      this.show_transaction_history = false
+    }  
+    else if(option == 'membership_details')
+    {
+      this.show_membership_details = true
+      this.show_dashboard = false
+      this.show_transaction_history = false
+    }
+    else if(option == 'transaction_history')
+    {
+      this.show_transaction_history = true
+      this.show_membership_details = false
+      this.show_dashboard = false
+    }
+
   }
 
 }
--- src/app/components/transaction-history-active/transaction-history-active.component.ts
+++ src/app/components/transaction-history-active/transaction-history-active.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TransactionService } from '../../services/transaction.service';
 
 @Component({
   selector: 'app-transaction-history-active',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionHistoryActiveComponent implements OnInit {
 
-  constructor() { }
+  //all_transactions : [ {Title:string, Author:string, Due_Date:Date, Renew_Count:Number, Fine: Number} ] 
+  active_transactions : any = []
+  constructor(public transaction: TransactionService) {
+    transaction.getActiveTransactions().subscribe(
+      res => {
+        this.active_transactions = res
+      },
+      err =>
+      {
+        console.log("Error")
+      }
+    )
+   }
 
   ngOnInit() {
+  }
+
+  renewBook(transaction_id:string)
+  {
+    this.transaction.renewTransaction(transaction_id).subscribe(
+      res => {
+
+      },
+      err =>
+      {
+
+      }
+    )
   }
 
 }
